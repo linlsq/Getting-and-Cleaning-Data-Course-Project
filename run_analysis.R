b8a5269cdf90a08a85231dd54082fa6452cc18a5
linlsq <linlsq5447168@sina.com>
2017-07-16 21:31:47 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -8,7 +8,10 @@ activityLabel   <- read.table("./activity_labels.txt",header=FALSE)
 subjectTrain    <-read.table("./train/subject_train.txt", header=FALSE)
 xTrain          <- read.table("./train/X_train.txt", header=FALSE)
 yTrain          <- read.table("./train/y_train.txt", header=FALSE)
-
+#Reading the test Data
+subjectTest    <-read.table("./test/subject_test.txt", header=FALSE)
+xTest         <- read.table("./test/X_test.txt", header=FALSE)
+yTest         <- read.table("./test/y_test.txt", header=FALSE)
 
 #Assign column names to the data above.
 
@@ -17,24 +20,14 @@ colnames(subjectTrain) <- "subId"
 colnames(xTrain) <- features[,2]
 colnames(yTrain) <- "activityId"
 
+colnames(subjectTest) <- "subId"
+colnames(xTest) <- features[,2]
+colnames(yTest) <- "activityId"
 
 #Merging training Data...
 
 trainData <- cbind(yTrain,subjectTrain,xTrain)
 
-#Reading the test Data
-
-subjectTest    <-read.table("./test/subject_test.txt", header=FALSE)
-xTest         <- read.table("./test/X_test.txt", header=FALSE)
-yTest         <- read.table("./test/y_test.txt", header=FALSE)
-
-# Assign column names.. same as for training data..
-
-colnames(subjectTest) <- "subId"
-colnames(xTest) <- features[,2]
-colnames(yTest) <- "activityId"
-
-# merging test Data
 testData <- cbind(yTest,subjectTest,xTest)
 
 
@@ -51,8 +44,7 @@ colNames <- colnames(finalData);
 # 2. Extract only the measurements on the mean and standard deviation for each measurement
 
 
-data_mean_std <-finalData[,grepl("mean|std|subject|activityId",colnames(finalData))]
-
+data_mean_std <-finalData[,grepl("mean|std|subId|activityId",colnames(finalData))]
 
 
 #3. #Uses descriptive activity names to name the activities in the data set
@@ -77,7 +69,7 @@ names(data_mean_std) <- make.names(names(data_mean_std))
 #add descriptive names
 
 names(data_mean_std) <- gsub("Acc", "Acceleration", names(data_mean_std))
- names(data_mean_std) <- gsub("^t", "Time", names(data_mean_std))
+names(data_mean_std) <- gsub("^t", "Time", names(data_mean_std))
 names(data_mean_std) <- gsub("^f", "Frequency", names(data_mean_std))
 names(data_mean_std) <- gsub("BodyBody", "Body", names(data_mean_std))
 names(data_mean_std) <- gsub("mean", "Mean", names(data_mean_std))
@@ -88,7 +80,7 @@ names(data_mean_std) <- gsub("Mag", "Magnitude", names(data_mean_std))
 #creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 
-tidydata_average_sub<- ddply(data_mean_std, c("subject","activity"), numcolwise(mean))
+tidydata_average_sub<- ddply(data_mean_std, c("subId","activityType"), numcolwise(mean))
 
 
-write.table(tidydata_average_sub,file="tidydata.txt")
+write.table(tidydata_average_sub,file="tidy.txt")
